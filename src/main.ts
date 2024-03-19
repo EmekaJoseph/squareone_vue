63695b518dda77000d4c62c3d52ab1193802787f
emekaJoseph <josephiwuji@gmail.com>
2024-03-19 13:29:53 +0100

    installation of tools

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+// @ts-nocheck
 
 //bootstrap@5.3.2
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -6,6 +7,13 @@ import 'bootstrap/dist/js/bootstrap.min.js'
 //bootstrap-icons
 import 'bootstrap-icons/font/bootstrap-icons.css'
 
+//vue-select@beta
+import vSelect from "vue-select";
+import "vue-select/dist/vue-select.css";
+
+// vue3-datepicker
+import VueDatePicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css'
 
 import './assets/main.css'
 
@@ -16,6 +24,9 @@ import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+
+app.component('VueDatePicker', VueDatePicker);
+app.component('vSelect', vSelect);
 
 app.use(createPinia())
 app.use(router)
